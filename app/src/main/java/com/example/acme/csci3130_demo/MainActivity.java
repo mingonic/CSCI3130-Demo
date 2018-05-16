406de0c3c562761d46bc305e3d252b5532eeaaa4
Nicholas Mingo <nic.mingo@icloud.com>
2018-05-16 15:48:51 -0300

    Completed assignment 1 by adding text box features

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void buttonOnClick(View v){
+
+        TextView textView = (TextView) findViewById(R.id.Display);
+
+        EditText editText = (EditText) findViewById(R.id.editText);
+
+        String value = editText.getText().toString();
+
+        textView.setText(value);
+
+
+        //Button button = (Button) v;
+        //EditText editText = (EditText) editText;
+        //((Button) v).setText("clicked");
+        //((EditText) i).setText("Test");
+    }
+
+
 }
